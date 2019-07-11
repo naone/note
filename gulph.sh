81688c92d33854043c09bbf7ca51bf981e119699
linuxisekai <galihwisnu18@gmail.com>
2019-07-12 04:11:33 +0700

    rebuilding site Jum Jul 12 04:11:33 WIB 2019

---
+++ gulph.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+echo "Initialize build tools, needs working NPM installation to run"
+npm install --save-dev sw-precache
+npm install --save-dev run-sequence
+npm install gulp-batch
+npm install gulp
