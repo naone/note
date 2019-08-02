819b9059dbb4b252cadd97b092e533304f46b214
linuxisekai <galihwisnu18@gmail.com>
2019-08-03 05:27:18 +0700

    rebuilding site Sab Agu  3 05:27:17 WIB 2019

--- themes/isekai/layouts/partials/core.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.button').on('click',function(){
-    $('.loader-wrapper').addClass('.is-active');
-    setTimeout(function(){
-      $('.loader-wrapper').removeClass('.is-active');
-    },3000)
-  })
-})
